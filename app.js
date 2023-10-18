ef36b152486cd6a7ce319ce53902bf3aa97dbf41
Sergey Napiev <napiev@mail.ru>
2023-10-19 01:19:56 +0300

    13PR fix review

--- app.js
+++ app.js
@@ -29,11 +29,11 @@ app.use((req, res, next) => {
 
 app.use(appRouter);
 
-app.use((err, req, res) => {
-  console.error(err.stack);
+app.use((req, res) => {
   res.status(HTTP_STATUS.NOT_FOUND).json({ message: 'Not Found' });
 });
 
+// Обработчик ошибки "Internal Server Error"
 app.use((err, req, res) => {
   console.error(err.stack);
   res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({ message: 'Internal Server Error' });
